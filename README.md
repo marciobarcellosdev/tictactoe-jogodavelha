7b8f13c0cd1066e45acab99c913a898ab0fea191
Marcio Barcellos <marcbrcx@gmail.com>
2024-10-05 10:34:17 -0300

    05/10/2024 10:34

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# tictactoe-jogodavelha
+# Jogo da velha (Tic-tac-toe)
+
+Jogo da velha (Tic-tac-toe) desenvolvido com HTML, JavaScript, CSS e Google Fonts.
+<br><br>
+Finalizado em 04/10/2024.
--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 var jogador1jogar = false;
 var jogador2jogar = false;
+var temganhador = false;
+var semganhador = false;
 var vx = "_";
 var matriz = [ [vx,vx,vx],[vx,vx,vx],[vx,vx,vx] ];
 var countcheia = 0;
 
 function GetText(cell) {
-  SetPlayer();
-  var tdElem = document.getElementById(cell);
-  tdElem.innerHTML = ValidatePlayerText();
-  var elementatual = document.getElementById('jogadoratual');
-  elementatual.innerHTML = ValidatePlayerTurn();
-  var elementlog = document.getElementById('log');
-  elementlog.innerHTML += GenerateLog(cell);
-  UpdateMatrix(cell, tdElem.innerHTML);
-  ValidateEmptyMatrix(countcheia);
-
+  if(!semganhador){
+    if(!temganhador){
+      SetPlayer();
+      var tdElem = document.getElementById(cell);
+      tdElem.innerHTML = ValidatePlayerText();
+      var elementatual = document.getElementById('jogadoratual');
+      elementatual.innerHTML = ValidatePlayerTurn();
+      var elementlog = document.getElementById('log');
+      elementlog.innerHTML += GenerateLog(cell);
+      UpdateMatrix(cell, tdElem.innerHTML);
+      ValidateEmptyMatrix(countcheia);
+      ValidateWin(tdElem.innerHTML);
+    }
+  }
 }
-
-// alinhar tudo center
-// validar quando ganha
 
 function SetPlayer() {
   if(!jogador1jogar && !jogador2jogar){
@@ -33,23 +36,27 @@ function SetPlayer() {
     jogador2jogar = false;
   }
 }
+
 function ValidatePlayerText() {
   if(jogador1jogar && !jogador2jogar) return '<azul>x</azul>';
   else if(jogador2jogar && !jogador1jogar) return '<vermelho>o</vermelho>';
 }
+
 function ValidatePlayerTurn() {
   if(jogador2jogar && !jogador1jogar) return '<azul>jogador 1 [X]</azul>';
   else if(jogador1jogar && !jogador2jogar) return '<vermelho>jogador 2 [O]</vermelho>';
 }
+
 function EmptyTable() {
   for (var i = 1; i < 10; i++) {
     const tdElem = document.getElementById("celula" + i);
     tdElem.innerHTML = '';
   }
-
-  SetPlayer();
+  jogador1jogar = false;
+  jogador2jogar = false;
+    
   var elementatual = document.getElementById('jogadoratual');
-  elementatual.innerHTML = ValidatePlayerTurn();
+  elementatual.innerHTML = '<azul>jogador 1 [X]</azul>';
 
   matriz = [ [vx,vx,vx],[vx,vx,vx],[vx,vx,vx] ];
   PrintEmptyMatrix();
@@ -57,9 +64,15 @@ function EmptyTable() {
   var elementlog = document.getElementById('log');
   elementlog.innerHTML = '';
 
+  var elementatual = document.getElementById('ganhador');
+  elementatual.innerHTML = '';
+
   document.getElementById("btnRestart").disabled = true;
   countcheia = 0;
+  temganhador = false;
+  semganhador = false;
 }
+
 function PrintEmptyMatrix() {
   var elementmatriz = document.getElementById('matriz');
   elementmatriz.innerHTML = '';
@@ -71,12 +84,21 @@ function PrintEmptyMatrix() {
   }
   elementmatriz.innerHTML += '<br><br>'; 
 }
+
 function GenerateLog(cell) {
   var atual;
   if(jogador2jogar && !jogador1jogar) atual = '<vermelho>jogador 2 [O]</vermelho>';
   else if(jogador1jogar && !jogador2jogar) atual = '<azul>jogador 1 [X]</azul>';
   return '<br>' + atual + ' jogou na ' + cell;
 }
+
+function ValidateEmptyMatrix(value) {
+  if (value === 9){
+    document.getElementById("btnRestart").disabled = false;
+    semganhador = true;
+  }
+}
+
 function UpdateMatrix(cell, value) {
   switch(cell) {
     case 'celula1':
@@ -116,12 +138,58 @@ function UpdateMatrix(cell, value) {
       countcheia++;
       break;
     default:
-      // code block
   }
   PrintEmptyMatrix();
 }
-function ValidateEmptyMatrix(value) {
-  if (value === 9){
+
+function ValidateWin(value) {
+  if ((matriz[0][0] === value) &&
+      (matriz[0][1] === value) &&
+      (matriz[0][2] === value)) {
+        temganhador = true;
+  }
+  if ((matriz[1][0] === value) &&
+      (matriz[1][1] === value) &&
+      (matriz[1][2] === value)) {
+        temganhador = true;
+  }
+  if ((matriz[2][0] === value) &&
+      (matriz[2][1] === value) &&
+      (matriz[2][2] === value)) {
+        temganhador = true;
+  }
+
+  if ((matriz[0][0] === value) &&
+      (matriz[1][0] === value) &&
+      (matriz[2][0] === value)) {
+        temganhador = true;
+  }
+  if ((matriz[0][1] === value) &&
+      (matriz[1][1] === value) &&
+      (matriz[2][1] === value)) {
+        temganhador = true;
+  }
+  if ((matriz[0][2] === value) &&
+      (matriz[1][2] === value) &&
+      (matriz[2][2] === value)) {
+        temganhador = true;
+  }
+
+  if ((matriz[0][0] === value) &&
+      (matriz[1][1] === value) &&
+      (matriz[2][2] === value)) {
+        temganhador = true;
+  }
+  if ((matriz[0][2] === value) &&
+      (matriz[1][1] === value) &&
+      (matriz[2][0] === value)) {
+        temganhador = true;
+  }
+  
+  if(temganhador){
     document.getElementById("btnRestart").disabled = false;
+
+    var elementganhador = document.getElementById('ganhador');
+    elementganhador.innerHTML = value === '<azul>x</azul>' ? 'jogador 1 venceu' : 'jogador 2 venceu';
   }
 }
